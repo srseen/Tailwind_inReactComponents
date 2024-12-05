b7a63ab5aa8b901badbffa78b257265ed4f9f0a3
srseen <seereen.2540@gmail.com>
2024-12-05 21:04:05 +0700

    Add config container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container: {
-      center: true,
-    },
+    // container: {
+    //   center: true,
+    // },
     extend: {
       colors: {
         "bg-primary": "#000000",
